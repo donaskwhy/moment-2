51c4484a39e2c5c5b14b48d4493690c0e890c995
donaskwhy <donaskwhy@nate.com>
2018-12-14 01:36:19 +0900

    follow unfollow js incomplete

--- app/controllers/user_alarms_controller.rb
+++ app/controllers/user_alarms_controller.rb
@@ -1,5 +1,5 @@
 class UserAlarmsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :set_post, only: [:delete_alarm, :allow_friend, :reject_friend]
 
   def show
     @user = User.find(params[:id])
@@ -7,12 +7,10 @@ class UserAlarmsController < ApplicationController
   end
 
   def delete_alarm
-    @user_alarm = UserAlarm.find(params[:id])
     @user_alarm.destroy
   end
 
   def allow_friend #친구 신청 수락
-    @user_alarm = UserAlarm.find(params[:id])
     @user1 = @user_alarm.user
     @user2 = @user_alarm.send_user
     if @user1.id > @user2.id ##중복생성 방지위해 항상 id값이 큰 것이 plyaer2_id에 저장되도록
@@ -28,12 +26,16 @@ class UserAlarmsController < ApplicationController
   end
 
   def reject_friend #친구 신청 거절
-    @user_alarm = UserAlarm.find(params[:id])
     @user_alarm.destroy
 
     respond_to do |format|
       format.js
     end
+  end
+
+  private
+  def find_user_alarm
+    @user_alarm = UserAlarm.find(params[:id])
   end
 
 end
