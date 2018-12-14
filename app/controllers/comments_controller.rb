d45f6f86d75a488fa7a46214a3bf2642831dd621
donaskwhy <donaskwhy@nate.com>
2018-12-14 22:20:55 +0900

    Ajax - comment ing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,11 @@ class CommentsController < ApplicationController
     comment.content = params[:content]
     comment.user_id = current_user.id
     comment.post_id = params[:post_id]
-    comment.save
+    if comment.save
+      respond_to do |format|
+        format.js
+      end
+    end
   end
 
   def destroy
